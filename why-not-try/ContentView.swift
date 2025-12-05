71545093029386af93026e5141d80939e9f4b84f
Zachary Lam <zlam@rrc.ca>
2025-12-05 14:04:23 -0600

    Initial Commit

---
+++ why-not-try/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  why-not-try
+//
+//  Created by Zachary Lâm on 2025-12-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ why-not-try/why_not_tryApp.swift
@@ -0,0 +1,17 @@
+//
+//  why_not_tryApp.swift
+//  why-not-try
+//
+//  Created by Zachary Lâm on 2025-12-05.
+//
+
+import SwiftUI
+
+@main
+struct why_not_tryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
