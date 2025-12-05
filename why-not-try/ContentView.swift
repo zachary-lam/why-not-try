2c830e3a90f0629f4fcd1b34034eb4d209d91f3d
Zachary Lam <zlam@rrc.ca>
2025-12-05 17:10:32 -0600

    Added randomness to selectedActivity and property wrapper to allow changing view state freely

--- why-not-try/ContentView.swift
+++ why-not-try/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
     
-    var selectedActivity = "Baseball" // hardcoded default
+    @State var selectedActivity = "Archery" // hardcoded default
     
     
     var body: some View {
@@ -33,7 +33,7 @@ struct ContentView: View {
             }
             
             Button("New Activity") {
-                // change activity
+                selectedActivity = activities.randomElement() ?? "Archery"
             }
             .buttonStyle(.borderedProminent)
         }
